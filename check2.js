5edf2177353827276af441df08aa18cfc225dbbf
YourCorrectUsername <your-email@example.com>
2026-07-26 12:41:42 +0100

    chore: remove debug/seed scripts

--- check2.js
+++
@@ -1,9 +0,0 @@
-// Check admin's org
-var admin = db.users.findOne({email: "anassamiri87@gmail.com"});
-print("Admin org: " + JSON.stringify(admin.organization));
-
-// Check test patients' orgs
-var pts = db.patients.find({name:/Test/},{name:1,organization:1}).toArray();
-pts.forEach(function(p) {
-  print(p.name + " | org: " + JSON.stringify(p.organization));
-});
--- verify.js
+++
@@ -1,2 +0,0 @@
-print("Patients with instructions: " + db.patients.countDocuments({"callInstructions.templateName": {$ne: ""}}));
-print("Scheduled calls: " + db.calls.countDocuments({status: "scheduled"}));
